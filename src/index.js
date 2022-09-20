4b3fe0985b211d5347dfb0a2a6d619a1ea8e2e8b
Dani <d.tarazoma@geekshubs.com>
2022-09-20 16:35:28 +0200

    feat: port constant

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+const PORT = 3000;
+
 app.get('/', (req, res)=> {
     return res.json({
         success: true,
@@ -16,4 +18,6 @@ app.get('/api/railway-tests', (req, res)=> {
     })
 });
 
-app.listen(3000, () => console.log("Server is running"));
+
+
+app.listen(PORT, () => console.log("Server is running:" + PORT));
